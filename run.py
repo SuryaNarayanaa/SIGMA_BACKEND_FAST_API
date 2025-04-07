df226cf6d976d88b775b74b7d68e1bef69bc52cd
Surya Narayanaa N T <thangarajsurya001@gmail.com>
2025-04-07 20:13:07 +0530

    EXISTING API IMPLEMENTATION

--- run.py
+++ run.py
@@ -3,3 +3,4 @@ import uvicorn
 if __name__ == "__main__":
     # The server port was intentionally changed from 8000 to 5000 for integration purposes.
     uvicorn.run("app.main:app", host="0.0.0.0", port=5000, reload=True)
+
